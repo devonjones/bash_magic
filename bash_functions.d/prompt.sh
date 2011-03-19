63d7748133d38f0f70ea58aeb00a220e53e78c8e
Devon Jones <devon@neptune.(none)>
2011-03-19 14:38:53 -0400

    defaulting to using $HOME instead of ~.  Fixed override issues in prompt.sh that was breaking eternal_history.sh

--- bash_functions.d/prompt.sh
+++ bash_functions.d/prompt.sh
@@ -32,7 +32,7 @@ unset color_prompt force_color_prompt
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*)
-	PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
+	PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
 	;;
 *)
 	;;
