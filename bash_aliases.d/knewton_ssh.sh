cf33f5ef6ce2d3bdd1f7017a1adff411f56350b0
Devon Jones <devon.jones@gmail.com>
2011-09-13 13:46:38 -0400

    removed one file and updated some ssh stuff

--- bash_aliases.d/knewton_ssh.sh
+++
@@ -1,3 +0,0 @@
-alias awsssh='awsssh.sh'
-alias kssh="$HOME/Knewton/utils/cmdline/bin/knewton_aws_helper.py"
-
--- bash_functions.d/knewton_ssh.sh
+++ bash_functions.d/knewton_ssh.sh
@@ -1,5 +1,8 @@
 export SSH_AGENT_PID=
 export SSH_AUTH_SOCK=
 alias awsssh='awsssh.sh'
-alias kssh="/home/devon/Knewton/utils/cmdline/bin/knewton_aws_helper.py"
+alias kssh="$HOME/Knewton/utils/cmdline/bin/knewton_ssh_helper.py"
+alias pssh="$HOME/Knewton/utils/cmdline/bin/knewton_ssh_helper.py -p"
+alias sssh="$HOME/Knewton/utils/cmdline/bin/knewton_ssh_helper.py -s"
+alias ussh="$HOME/Knewton/utils/cmdline/bin/knewton_ssh_helper.py -u"
 
