d7247443a46d9ad3da2331312f15849e8fb6e49c
Devon Jones <devon@knewton.com>
2012-05-02 11:18:48 -0400

    adding some work paths

---
+++ bash_functions.d/knewton_java.sh
@@ -0,0 +1,2 @@
+export PATH=$PATH:$HOME/apps/apache-maven/bin
+
--- bash_functions.d/knewton_python.sh
+++ bash_functions.d/knewton_python.sh
@@ -1,1 +1,6 @@
 export PYTHONPATH=.:$HOME/knewton/lib/python
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]
+then
+	export WORKON_HOME=/opt/virtualenvs
+	source /usr/local/bin/virtualenvwrapper.sh
+fi
