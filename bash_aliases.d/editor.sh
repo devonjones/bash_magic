914afc55e77137538c8f6e537ec89a7ea1bbecf6
Devon Jones <devon.jones@gmail.com>
2012-08-23 12:47:42 -0400

    fixing vim for OSX

--- bash_aliases.d/editor.sh
+++ bash_aliases.d/editor.sh
@@ -1,5 +1,4 @@
-alias ge='gedit'
-alias gi="/usr/bin/gvim --servername GVIM --remote"
+alias gi="/opt/local/bin/mvim --servername GVIM --remote"
 
 # Open last modified file in vim
 alias Vim="vim `ls -t | head -1`"
